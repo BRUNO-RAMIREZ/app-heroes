482aa11b1dd7221ac1ca3a516d1591c436d68235
BRUNO-RAMIREZ <89104842+BRUNO-RAMIREZ@users.noreply.github.com>
2023-01-20 17:19:06 -0400

    creando las rutas de heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroesRoutingModule } from './heroes-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
@@ -7,7 +8,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
 @NgModule({
-  declarations: [AgregarComponent, BuscarComponent, HeroeComponent, HomeComponent, ListadoComponent],
-  imports: [CommonModule],
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    HeroeComponent,
+    HomeComponent,
+    ListadoComponent,
+  ],
+  imports: [CommonModule, HeroesRoutingModule],
 })
 export class HeroesModule {}
