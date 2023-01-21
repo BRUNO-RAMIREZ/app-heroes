4eb398e4fcdc2207ac4bdaf844c0ef7c152514c8
BRUNO-RAMIREZ <89104842+BRUNO-RAMIREZ@users.noreply.github.com>
2023-01-21 10:37:45 -0400

    estableciendo las variables de entorno de desarrollo en los environments

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,18 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Heroe } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
-  constructor(private _http: HttpClient) {}
+  private _baseUrl: string;
+
+  constructor(private _http: HttpClient) {
+    this._baseUrl = environment.baseUrl;
+  }
 
   getHeroes(): Observable<Heroe[]> {
-    return this._http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this._http.get<Heroe[]>(`${this._baseUrl}/heroes`);
   }
 
   getHeroe(id: string): Observable<Heroe> {
-    return this._http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this._http.get<Heroe>(`${this._baseUrl}/heroes/${id}`);
   }
 }
